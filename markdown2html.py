c0ef4f980f7c4c41b978e6090f476e6092e7fe74
Capotedev <carolina.capote@imagineapps.co>
2022-04-30 11:28:40 -0500

    ADD - python script that take 2 argv

---
+++ markdown2html.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains script to work with the command line.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+    if os.path.exists(sys.argv[1]) is False:
+        sys.stderr.write("Missing {}".format(sys.argv[1]))
+        exit(1)
+    exit(0)
