4ed546e620f04a68764c663fd562fad1f751e3d1
Capotedev <carolina.capote@imagineapps.co>
2022-04-30 11:54:04 -0500

    CHANGE pritn() fot stderr

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import sys
     import os
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if os.path.exists(sys.argv[1]) is False:
