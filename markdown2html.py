5b6014150af385a9acc65d983e70c3506dd09b28
Capotedev <carolina.capote@imagineapps.co>
2022-05-01 12:03:57 -0500

    CHANGE mardown titles for html title tags

--- markdown2html.py
+++ markdown2html.py
@@ -13,4 +13,25 @@ if __name__ == "__main__":
     if os.path.exists(sys.argv[1]) is False:
         sys.stderr.write("Missing {}\n".format(sys.argv[1]))
         exit(1)
+
+    html_titles = {
+        "######": ["<h6>", "</h6>"],
+        "#####": ["<h5>", "</h5>"],
+        "####": ["<h4>", "</h4>"],
+        "###": ["<h3>", "</h3>"],
+        "##": ["<h2>", "</h2>"],
+        "#": ["<h1>", "</h1>"]
+    }
+
+    html_file = open(sys.argv[2], 'w')
+    with open(sys.argv[1], "r") as md_file:
+        lines = md_file.readlines()
+        for line in lines:
+            if line.startswith("#"):
+                for title in html_titles:
+                    if line.startswith(title):
+                        html_file.write(html_titles[title][0] +
+                                        line[len(title):-1] +
+                                        html_titles[title][1] + "\n")
+                        break
     exit(0)
