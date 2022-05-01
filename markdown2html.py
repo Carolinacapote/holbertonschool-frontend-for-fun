2702a0dd4da95ff484c143615b6f3e1f8b114710
Capotedev <carolina.capote@imagineapps.co>
2022-05-01 12:30:00 -0500

    CHANGe markdown ul for html ul tags

--- markdown2html.py
+++ markdown2html.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
         "#": ["<h1>", "</h1>"]
     }
 
+    html_li = ["<li>", "</li>"]
+    new_ul = "<ul>\n"
     html_file = open(sys.argv[2], 'w')
     with open(sys.argv[1], "r") as md_file:
         lines = md_file.readlines()
@@ -31,7 +33,11 @@ if __name__ == "__main__":
                 for title in html_titles:
                     if line.startswith(title):
                         html_file.write(html_titles[title][0] +
-                                        line[len(title):-1] +
+                                        line[len(title) + 1:-1] +
                                         html_titles[title][1] + "\n")
                         break
+            elif line.startswith("-"):
+                new_ul += "\t" + html_li[0] + line[2:-1] + html_li[1] + "\n"
+    if len(new_ul) > 5:
+        html_file.write(new_ul + "</ul>\n")
     exit(0)
