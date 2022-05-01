f3271a1a9bc9ca22e5b6f4ee0040362ba503638d
Capotedev <carolina.capote@imagineapps.co>
2022-05-01 16:33:33 -0500

    FIx file

--- markdown2html.py
+++ markdown2html.py
@@ -30,8 +30,7 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], "r") as md_file:
         lines = md_file.readlines()
-        for i in range(len(lines)):
-            line = lines[i]
+        for line in lines:
             if line.startswith("#"):
                 for title in html_titles:
                     if line.startswith(title):
@@ -39,30 +38,6 @@ if __name__ == "__main__":
                                         line[len(title) + 1:-1] +
                                         html_titles[title][1] + "\n")
                         break
-            elif line.startswith("- "):
-                new_ul += "<li>" + line[2:-1] + "</li>\n"
-            elif line.startswith("* "):
-                new_ol += "<li>" + line[2:-1] + "</li>\n"
-            else:
-                if i == 0 or lines[i - 1] == "\n":
-                    if i == len(lines) - 1 or lines[i + 1] == "\n":
-                        new_paragraph += "<p>\n" + line + "</p>\n"
-                    else:
-                        new_paragraph += "<p>\n" + line + "<br/>\n"
-                        j = i + 1
-                        while j < len(lines) and lines[j] != "\n":
-                            new_paragraph += lines[j]
-                            if j < len(lines) - 1:
-                                new_paragraph += "<br/>\n"
-                            j += 1
-                        new_paragraph += "</p>\n"
-
-    if len(new_ul) > 5:
-        html_file.write(new_ul + "</ul>\n")
-    if len(new_ol) > 5:
-        html_file.write(new_ol + "</ol>\n")
-    if new_paragraph != "":
-        html_file.write(new_paragraph)
 
     html_file.close()
     md_file.close()
